6726ba90dcdf7156e7e5b212c1d3b6a119dc749f
ese-omene <55164935+ese-omene@users.noreply.github.com>
2019-12-07 21:09:58 -0500

    game working
    js for game now works

--- js/scriptGame.js
+++ js/scriptGame.js
@@ -5,7 +5,7 @@ var coconut = document.getElementById('coconut');
 coconut.onclick = moveCoconut;
 
  function moveCoconut(){
-   alert("you clicked the coconut");
+  // alert("you clicked the coconut");
 var x = event.pageX;
 
 var y = event.pageY;
@@ -14,31 +14,29 @@ var stageHeight = $('#stage').height()
 var stageWidth = $('#stage').width()
 
 
-//var stage = document.getElementById('stage')
-
-//var stageHeight = stage.height();
-//var stageHeight = stage.height()
-
-
-
-
-
 var xMove =  Math.random()*1000
 var yMove =  Math.random() *1000
 
+console.log("the stage is " + stageWidth + "wide and " + stageHeight + " in height")
+console.log("the coconut moved " + xMove+ "left and "+ yMove + "up/down")
 
-console.log(xMove, stageWidth, yMove, stageHeight)
-if (yMove<stageWidth&&xMove<stageHeight){
-
-  coconut.style.left = yMove + 'px';
-coconut.style.top = xMove + 'px';
-}else{
-
-  coconut.style.left = (yMove - stageHeight) + 'px';
-coconut.style.top = (xMove - stageWidth) + 'px';
+if (stageHeight>yMove){
+// 300>250
+coconut.style.top = yMove + 'px';
+}else if (yMove<500){
+//  400
+coconut.style.top = (yMove - stageHeight) + 'px';
+} else {
+coconut.style.top =  ( Math.floor(yMove)/2.5) - stageHeight + 'px';
 }
 
-
+if (stageWidth>xMove){
+  // 1360>295
+  coconut.style.left = xMove + 'px';
+}else{
+// 1360 -340 no no take cant stage away from move since bigger
+coconut.style.left = (xMove - stageWidth) + 'px';
+}
 }
 
 });
