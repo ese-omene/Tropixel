3b802fe2d3ce7da9be8ae627c0ab4a7179945aa5
shikhagoyal96 <55159778+shikhagoyal96@users.noreply.github.com>
2019-12-08 14:10:33 -0500

    Update scriptCareers.js

--- js/scriptCareers.js
+++ js/scriptCareers.js
@@ -2,14 +2,14 @@
 
 jQuery(document).ready(function(){
 	
-	$('.contentBox').hide();
+	$('.jobDetails').hide();
 	
 	$('h3').dblclick(function(){
-		$(this).next('.contentBox').slideDown().show(20000);
+		$(this).next('.jobDetails').slideDown().show(20000);
 	});
 	
 	$('h3').click(function(){
-		$('.contentBox').slideUp().hide(20000);
+		$('.jobDetails').slideUp().hide(20000);
 	});
 	
 });
